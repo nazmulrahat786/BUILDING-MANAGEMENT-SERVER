74027c37aa6a56b7ffee0e6ea15349b626633af0
nazmulrahat786 <nazmulrahat786.13@gmail.com>
2025-09-13 23:06:19 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,368 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors');
+var jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express()
+const stripe = require('stripe')(process.env.Stripe_Key)
+const port = process.env.PORT || 5000
+
+// console.log(process.env.ACCESS_TOKEN_SECRET)
+//middleware 
+app.use(express.json())
+app.use(cors())
+//middleware
+const varifyToken = (req, res, next) => {
+  // console.log("varify token: ", req.headers)
+  // console.log("varify token: ", req.headers.authorization)
+  if (!req.headers.authorization) {
+    return res.status(401).send({ messages: 'forbiden assess' })
+  }
+  const token = req.headers.authorization.split(' ')[1];
+  // console.log(token)
+  // console.log(token)
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(401).send({ messages: 'forbiden assess' })
+    }
+    req.decoded = decoded
+    // console.log(decoded)
+    next()
+  });
+}
+
+//mongodb connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oottvnu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    //database collection
+    const userCollection = client.db('BuildingManagement').collection('users')
+    const apartmentCollection = client.db('BuildingManagement').collection('apartment')
+    const ageementCollection = client.db('BuildingManagement').collection('ageements')
+    const announcementCollection = client.db('BuildingManagement').collection('announcements')
+    const ageementAcceptCollection = client.db('BuildingManagement').collection('Acceptapartment')
+    const cuponsCollection = client.db('BuildingManagement').collection('cupons')
+    const paymentCollection = client.db('BuildingManagement').collection('paymentHistory')
+    //jwt 
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30d' })
+
+      res.send({ token })
+    })
+    //payment history
+    app.post('/paymentHistory', async (req, res) => {
+      const history = req.body
+      const result = await paymentCollection.insertOne(history)
+      console.log(history)
+      console.log(result)
+      res.send(result)
+    })
+    app.get('/paymentHistory', varifyToken, async (req, res) => {
+      const email = req?.query?.email
+      const query = { userEmail: email }
+      // console.log("email",email)
+      const result = await paymentCollection.find(query).toArray()
+      res.send(result)
+    })
+    // get all payment history
+    app.get('/allpaymentHistory', varifyToken, async (req, res) => {
+      // const email = req?.query?.email
+      // const query = { userEmail: email }
+      // console.log("email",email)
+      const result = await paymentCollection.find().toArray()
+      res.send(result)
+    })
+   
+    //cupon collection
+    //update cupons
+    app.put('/cupons/:id', async (req, res) => {
+      const id = req.params
+      const couponsInFo = req.body
+      const filter = { _id: new ObjectId(id) }
+      const updateDoc = {
+        $set: {
+          cuponCode: couponsInFo.cuponCode,
+          discount: couponsInFo.discount,
+          description: couponsInFo.description
+        },
+      };
+      const result = await cuponsCollection.updateOne(filter, updateDoc);
+
+      console.log(result)
+      res.send(result)
+    })
+    //delete cupons
+    app.delete('/cupons/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await cuponsCollection.deleteOne(query)
+      res.send(result)
+    })
+
+    //get cupon
+    app.get('/cupons', async (req, res) => {
+      const result = await cuponsCollection.find().toArray()
+      res.send(result);
+      // console.log(result)git
+    })
+    //post cupon
+    app.post('/cupons', varifyToken, async (req, res) => {
+      const cupon = req.body;
+      console.log('cupon', cupon)
+      const result = await cuponsCollection.insertOne(cupon);
+      res.send(result)
+      // console.log(result)
+    })
+    //payment intent
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100)
+      console.log(amount)
+      // Create a PaymentIntent with the order amount and currency
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+        payment_method_types: [
+          "card"
+        ],
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
+    //announcement Collection
+    app.get('/announcements', async (req, res) => {
+      const result = await announcementCollection.find().toArray()
+      res.send(result)
+      // console.log(result)
+    })
+    //post users details
+    app.post('/users', async (req, res) => {
+      let result = "Alreary added this user";
+      const users = req.body;
+      const userEmail = users.email;
+      const query = { email: userEmail };
+      const findUser = await userCollection.findOne(query);
+      if (!findUser) {
+        result = await userCollection.insertOne(users)
+      }
+      // console.log(result);
+      res.send(result);
+    })
+    //get users details
+    app.get('/users', async (req, res) => {
+      const findUser = await userCollection.find().toArray();
+      // console.log(findUser);
+      res.send(findUser);
+    })
+    //check admin
+    app.get('/user/admin/:email', varifyToken, async (req, res) => {
+      const email = req.params.email;
+      // console.log(email)
+      // console.log(req.decoded.email)
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ massages: 'unauthorized access' })
+      }
+      const query = { email: email }
+
+      const user = await userCollection.findOne(query);
+      let role = false;
+      // let member = false;
+      // console.log(user)
+      if (user?.role === 'admin') {
+        role = 'admin';
+
+      } else if (user?.role === 'member') {
+        role = 'member'
+      } else {
+        role = 'user'
+      }
+      res.send({ role });
+      // console.log({ role })
+    })
+
+
+    //get agreements request
+    app.get('/agreementsRequest', varifyToken, async (req, res) => {
+      const agreementsUser = await ageementCollection.find().toArray();
+      // console.log(agreementsUser);
+      res.send(agreementsUser);
+    })
+    //update agreements request
+    app.patch('/agreementsRequest', varifyToken, async (req, res) => {
+      const id = req.query.id;
+      const useremail = req.query.email;
+      // console.log(id,email)
+      const filter = { _id: new ObjectId(id) }
+      console.log(filter)
+      const currentDate = new Date()
+      const formattedDate = currentDate.toLocaleDateString('en-US', {
+        month: '2-digit',
+        day: '2-digit',
+        year: 'numeric',
+      });
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          status: 'checked',
+        },
+      };
+      // const result =await ageementCollection.findOne(filter)
+      const result = await ageementCollection.updateOne(filter, updateDoc, options);
+
+      if (result.modifiedCount) {
+        const query = { email: useremail }
+        const options = { upsert: true };
+        const updateDoc = {
+          $set: {
+            role: 'member'
+          },
+        };
+        const userresult = await userCollection.updateOne(query, updateDoc, options);
+        if (userresult) {
+          const acceptRequest = await ageementCollection.findOne(filter)
+
+
+          const requestinfo = {
+            requetsId: acceptRequest?._id,
+            userName: acceptRequest?.userName,
+            userEmail: acceptRequest?.userEmail,
+            floorNo: acceptRequest?.floorNo,
+            apartmentNo: acceptRequest?.apartmentNo,
+            blockName: acceptRequest?.blockName,
+            rent: acceptRequest?.rent,
+            requestDate: acceptRequest?.requestDate,
+            RoomNo: acceptRequest?.roomNo,
+            // acceptRequest,
+            AcceptedDate: formattedDate,
+          }
+          const insertData = await ageementAcceptCollection.insertOne(requestinfo)
+          // console.log('userRole', userresult)
+          res.send({ acceptStatus: result, userStasus: userresult, insertData })
+        }
+      }
+      // console.log(result)
+    })
+    //handle reject request
+    app.patch('/agreementsRejectRequest', varifyToken, async (req, res) => {
+      const id = req.query.id;
+      // console.log(id,email)
+      const filter = { _id: new ObjectId(id) }
+      // console.log(filter)
+
+      const updateDoc = {
+        $set: {
+          status: 'checked'
+        },
+      };
+      // const result =await ageementCollection.findOne(filter)
+      const result = await ageementCollection.updateOne(filter, updateDoc);
+
+      console.log("update user:", result)
+      res.send(result)
+    })
+
+    //manage member
+    app.patch('/userRole', varifyToken, async (req, res) => {
+      const id = req.query.id;
+      console.log(id)
+      const filter = { _id: new ObjectId(id) }
+      console.log(filter)
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          role: 'user'
+        },
+      };
+      const userresult = await userCollection.updateOne(filter, updateDoc, options);
+      res.send(userresult)
+      // console.log(userresult)
+
+    })
+    //get apartmentdata
+    app.get('/apartment', async (req, res) => {
+      const result = await apartmentCollection.find().toArray()
+      res.send(result)
+      // console.log(result)
+    })
+    //get apartmentdata for navagation
+    app.get('/pagination', async (req, res) => {
+      const page = parseInt(req.query.page)
+      const size = parseInt(req.query.size)
+
+      const result = await apartmentCollection.find()
+        .skip(page * size)
+        .limit(size)
+        .toArray();
+      console.log(result)
+      res.send(result)
+      // console.log(result)
+    })
+    //post user Ageement 
+    app.post('/ageement', async (req, res) => {
+      const ageementData = req.body;
+      const result = await ageementCollection.insertOne(ageementData)
+      // console.log(result)
+      res.send(result)
+    })
+    //find user Ageement of specific user 
+    app.get('/bookedRoom', async (req, res) => {
+
+      const result = await ageementAcceptCollection.find().toArray()
+      // console.log("booking Room:", result)
+      res.send(result)
+    })
+    app.get('/ageementuser/:email', varifyToken, async (req, res) => {
+      const email = req.params.email;
+      const query = { userEmail: email }
+      // console.log(email)
+      // console.log(query)
+      const result = await ageementAcceptCollection.findOne(query)
+      // console.log(result)
+      res.send(result)
+    })
+    //make Announcement
+    app.post('/makeannouncement', varifyToken, async (req, res) => {
+      const announcementInfo = req.body
+      const result = await announcementCollection.insertOne(announcementInfo)
+      res.send(result)
+      // console.log(result)
+    })
+
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+//connecting server
+app.get('/', (req, res) => {
+  res.send('server is connecting')
+})
+
+//runing port
+app.listen(port, () => {
+  console.log('running  port is : ', port)
+})
