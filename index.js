378fc54e1b8628367eef65b950206dc43a8343d4
Nazmul Haque Rahat <nazmulrahat786.13@gmail.com>
2025-09-16 01:24:26 +0600

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const varifyToken = (req, res, next) => {
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oottvnu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 
-
+ 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
